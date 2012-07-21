14a517a7ffaca7babc98d88379fe07f6e7a5954f
NewPlayer <karasztamas@gmail.com>
2012-07-21 20:17:27 +0200

    init

---
+++ plugins/e107/anipalace-widget-magazin/readme.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * AniPalace Widget-Magazin e107 plugin
+ * 
+ * @link https://github.com/AniPalace/Widget-Magazin
+ * @author NewPlayer
+ * 
+ */
+
+header( "Location: https://github.com/AniPalace/Widget-Magazin/blob/master/README.md" );
+die();
+?>
