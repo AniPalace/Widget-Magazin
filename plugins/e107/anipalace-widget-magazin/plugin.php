83264fc649ed36cee8871fa48c301892c2f8b727
NewPlayer <karasztamas@gmail.com>
2012-07-21 16:53:49 +0200

    e107 plugin init

---
+++ plugins/e107/anipalace-widget-magazin/plugin.php
@@ -0,0 +1,22 @@
+<?php
+
+$eplug_name = "AniPalace Widget-Magazin";
+$eplug_version = "1.0";
+$eplug_url = "https://github.com/AniPalace/Widget-Magazin";
+
+$eplug_description = "AniMagazin és AniStation widget megjelenítő.";
+$eplug_readme = "https://github.com/AniPalace/Widget-Magazin/blob/master/README.md";
+
+$eplug_author = "NewPlayer";
+$eplug_email = "newplayer@anipalace.hu";
+
+$eplug_folder = "anipalace-widget-magazin";
+$eplug_icon = $eplug_folder."/images/icon_32.png";
+$eplug_icon_small = $eplug_folder."/images/icon_16.png";
+$eplug_compatible = "e107v0.7+";
+$eplug_compliant = TRUE;
+
+$eplug_caption = "AniPalace";
+
+$eplug_done = "Installation Successful...";
+?>
