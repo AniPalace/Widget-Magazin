1a359c58b9708de357f104ecf51a483afeb4c9e6
NewPlayer <karasztamas@gmail.com>
2012-07-21 20:18:40 +0200

    Commentek

--- plugins/e107/anipalace-widget-magazin/anipalace-widget-magazin_menu.php
+++ plugins/e107/anipalace-widget-magazin/anipalace-widget-magazin_menu.php
@@ -1,17 +1,29 @@
 <?php
+/**
+ * AniPalace Widget-Magazin e107 plugin
+ * 
+ * @link https://github.com/AniPalace/Widget-Magazin
+ * @author NewPlayer
+ * 
+ */
 
-if (!defined('e107_INIT')) {
-	exit;
-}
 
+if (!defined('e107_INIT')) { exit; }
+
+// a widget címe
 $caption = "AniPalace";
-$text = '<div class="anipalace-widget" data-type="all" data-style="changer"></div>
+
+// a widget tartalma
+//TODO: van $footer_js e107-ben de az nem akart működni
+$text = '
+<div class="anipalace-widget" data-type="all" data-style="changer"></div>
 <script type="text/javascript" src="' . e_PLUGIN_ABS . 'anipalace-widget-magazin/anipalace-widget-magazin.js"></script>
 <script type="text/javascript">
 	getAniPalaceWidgetMagazin( );
 </script>
 ';
 
+// Widget megjelenítése
 $ns->tablerender($caption, $text, 'anipalace-widget-magazin');
 
 ?>
